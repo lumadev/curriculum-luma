ca2be996bd9ff3e046216910c577dac2792e20fe
Luma Macagnan <lumamacagnan@gmail.com>
2026-02-25 21:15:25 -0300

    set flat config for es lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,35 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import ts from "@typescript-eslint/eslint-plugin";
-import tsRecommended from "@typescript-eslint/eslint-plugin/dist/configs/recommended.js";
+import js from "@eslint/js"
+import ts from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
 
-export default {
-  ignores: ["dist"],
-  extends: [js.configs.recommended, "plugin:@typescript-eslint/recommended"],
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default [
+  {
+    ignores: ["dist"],
   },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-    "@typescript-eslint": ts,
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-unused-vars": "off",
-  },
-};
+]
