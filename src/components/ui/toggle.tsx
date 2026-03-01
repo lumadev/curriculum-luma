132211adda872dd719fc1e6adf9668e9e154a578
Luma Macagnan <lumamacagnan@gmail.com>
2026-03-01 16:20:00 -0300

    fix deprecation error: replace ElementRef with ComponentRef

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import { toggleVariants } from "@/components/ui/toggle-variants";
 
 const Toggle = React.forwardRef<
-  React.ElementRef<typeof TogglePrimitive.Root>,
+  React.ComponentRef<typeof TogglePrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>
 >(({ className, variant, size, ...props }, ref) => (
   <TogglePrimitive.Root ref={ref} className={cn(toggleVariants({ variant, size, className }))} {...props} />
