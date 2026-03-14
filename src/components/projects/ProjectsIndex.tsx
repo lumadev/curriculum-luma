8649f0bf6e5ff53e79c03a48fb91d3ce01169154
Luma Macagnan <lumamacagnan@gmail.com>
2026-03-14 17:45:29 -0300

    separate array of projects in new file

--- src/components/projects/ProjectsIndex.tsx
+++ src/components/projects/ProjectsIndex.tsx
@@ -9,44 +9,16 @@ import {
 } from "@/components/ui/dialog";
 
 import { Separator } from "@/components/ui/separator";
+import { projects, Project } from "./projectsData";
 
 import ProjectCard from "./ProjectCard";
 import ProjectsHeader from "./ProjectsHeader";
 import ExtraImagesGallery from "./ExtraImagesGallery";
 import ProjectDetailsLeft from "./ProjectDetailsLeft";
 
-interface Project {
-  title: string;
-  description: string;
-  fullDescription: string;
-  tags: string[];
-  image: string;
-  galleryImages: string[];
-}
-
 const Projects = () => {
   const [selected, setSelected] = useState<Project | null>(null);
   const [lightboxImage, setLightboxImage] = useState<string | null>(null);
-
-  const projects: Project[] = [
-    {
-      title: "Music Lesson Scheduler",
-      description:
-        "Application created to simplify class replacement management for music teachers.",
-      fullDescription:
-        "Application created to help music teachers manage students, " +
-        "class replacements, and lesson organization in a centralized and intuitive interface. " +
-        "The system improves scheduling control, tracks student progress through assigned songs, " +
-        "and keeps event dates organized — all in one place. " +
-        "It is fully responsive, providing a seamless experience across desktop, tablet, and mobile devices.",
-      tags: ["React", "Node.js", "Express.js"],
-      image: "/makeupProject/makeups-login.png",
-      galleryImages: [
-        "/makeupProject/makeups-list.png",
-        "/makeupProject/initial-page.png",
-      ],
-    },
-  ];
 
   return (
     <>
