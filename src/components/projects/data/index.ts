310bc931f831e8129eea641be9f4660b6fe0ef79
Luma Macagnan <lumamacagnan@gmail.com>
2026-03-16 11:15:36 -0300

    add notifeye project to the portfolio

--- src/components/projects/data/index.ts
+++ src/components/projects/data/index.ts
@@ -2,6 +2,7 @@ import { Project } from "./projectTypes";
 
 import { musicProject } from "./musicProject";
 import { merchantProject } from "./merchantProject";
+import { notifeyeProject } from "./notifeyeProject";
 import { realEstateProject } from "./realEstateProject";
 import { senaiItinerariosProject } from "./senaiItinerariosProject";
 import { mundoSenaiDocenteProject } from "./mundoSenaiDocenteProject";
@@ -9,6 +10,7 @@ import { mundoSenaiDocenteProject } from "./mundoSenaiDocenteProject";
 export const projects: Project[] = [
   musicProject,
   merchantProject,
+  notifeyeProject,
   senaiItinerariosProject,
   mundoSenaiDocenteProject,
   realEstateProject
