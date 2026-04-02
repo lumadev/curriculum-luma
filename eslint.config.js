557c1deaaacc0be37604764023df5acf0b158239
Luma Macagnan <lumamacagnan@gmail.com>
2026-04-02 00:49:43 -0300

    space fixes

--- eslint.config.js
+++ eslint.config.js
@@ -25,8 +25,7 @@ export default [
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+        "warn", { allowConstantExport: true },
       ],
       "@typescript-eslint/no-unused-vars": "off",
     },
