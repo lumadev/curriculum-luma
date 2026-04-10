164197c20b2be313cebb9e92ac9b84973af03341
Luma Macagnan <lumamacagnan@gmail.com>
2026-04-10 18:21:00 -0300

    create initial ecommerce project structure

---
+++ src/components/projects/data/personal/ecommerce-3d.ts
@@ -0,0 +1,14 @@
+import { Project } from "../projectTypes";
+
+export const ecommerce3dProject: Project = {
+  id: "ecommerce3d",
+  tags: ["TypeScript", "React", "PostgreSQL", "Node.js", "NestJS", "Ecommerce"],
+  image: "/ecommerce3d/ecommerce.png",
+  galleryImages: [
+    "/ecommerce3d/ecommerce.png",
+    "/ecommerce3d/categories.png",
+    "/ecommerce3d/custom-order.png",
+    "/ecommerce3d/product-details.png",
+  ],
+  isFavorite: true,
+};
